ded224f46a88a113d6a3ca3ee466b20d88f5afe6
Jack <hungjack1222@gmail.com>
2025-03-25 00:55:23 +0800

    feat: add routing and layout components for Vortex Verdict application. Create header, footer and home page prototype

--- vortex-verdict/src/main.js
+++ vortex-verdict/src/main.js
@@ -2,6 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import "@/assets/css/bootstrap.css"
+import '@fortawesome/fontawesome-free/css/all.css';
 
-createApp(App).mount('#app')
+import router from "./router.js"
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
---
+++ vortex-verdict/src/pages/TheHome.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="home">
+        <div class="container mt-4">
+
+            <h1 class="text-center">Vortex Verdict 旋渦裁決</h1>
+            <p>成立於2025年3月19日，
+                ---
+                Vortex Verdict 旋渦裁決｜遊戲評價的新標準
+
+                在遊戲世界裡，資訊就像漩渦，真假難辨、褒貶不一。Vortex Verdict 旋渦裁決，正是為了打破混沌而誕生——我們專注於專業、客觀且深度的遊戲評價，提供玩家最值得信賴的遊戲指引。
+
+                我們在做什麼？
+
+                我們不只是寫評論，我們做的是「裁決」。
+                從遊戲性、劇情、操作手感、美術風格、音樂設計到市場潛力，Vortex Verdict 採用獨家的「旋渦評分系統（Vortex Rating
+                System）」為每款遊戲進行科學化分析與玩家感受綜合評比，讓每一個分數都有數據、有溫度。</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.home {
+    width: 100%;
+    height: 90vh;
+    background-color: #fcfcfc;
+}
+</style>
