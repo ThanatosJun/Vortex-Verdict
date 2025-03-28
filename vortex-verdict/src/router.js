dbfc0257cafb0ff2ca3e60b446831d5bd7d10092
Jack <hungjack1222@gmail.com>
2025-03-29 03:16:39 +0800

    feat: add GameReview page and update routing for game details

--- vortex-verdict/src/router.js
+++ vortex-verdict/src/router.js
@@ -5,18 +5,19 @@ import NotFound from "./pages/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: "/", component: Home },
+  routes: [
+    { path: "/", component: Home },
     { path: "/:notFound(.*)", component: NotFound },
     {
-      path: '/games',
-      name: 'GameList',
-      component: () => import('@/pages/GameList.vue')
+      path: "/games",
+      name: "GameList",
+      component: () => import("@/pages/GameList.vue"),
     },
     {
-      path: '/game/:id',
-      name: 'GameReview',
-      component: () => import('@/pages/GameReview.vue')
-    }
+      path: "/game/:id",
+      name: "GameReview",
+      component: () => import("@/pages/GameReview.vue"),
+    },
   ],
 });
 
